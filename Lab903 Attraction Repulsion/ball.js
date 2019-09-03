b9440944dde53d379f565f84564b82b67c6adab4
798307 <54591188+798307@users.noreply.github.com>
2019-09-03 08:28:46 -0700

    lab828 into lab903

---
+++ Lab903 Attraction Repulsion/ball.js
@@ -0,0 +1,37 @@
+//Andrew Matel
+//09/03/19
+//Attraction Repulsion
+class Ball{
+  constructor(x, y, dx, dy){
+    this.loc = createVector(x,y);
+    this.vel = createVector(dx,dy);
+    this.clr = color(random(255),random(255),random(255));
+    this.w = random(10,70);
+  }
+  run(){
+    this.checkEdge();
+    this.update();
+    this.render();
+  }
+  checkEdge(){
+    if(this.loc.x < 0){
+      this.vel.dx = -this.vel.dy;
+    }
+    if(this.loc.x > width){
+      this.vel.dx = -this.vel.dx;
+    }
+    if(this.loc.y < 0){
+      this.vel.dy = -this.vel.dy;
+    }
+    if(this.loc.y > height){
+      this.vel.dy = -this.vel.dy;
+    }
+  }
+  update(){
+    this.loc.add(this.vel);
+  }
+  render(){
+    fill(this.clr);
+    ellipse(this.loc.x, this.loc.y, this.w, this.w);
+  }
+}
