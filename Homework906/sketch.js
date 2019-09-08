1a827f60b810da515528cf4b1a308e26655743fa
798307 <54591188+798307@users.noreply.github.com>
2019-09-07 20:32:40 -0700

    Quiz Questions

--- Homework906/sketch.js
+++ Homework906/sketch.js
@@ -15,3 +15,13 @@ function setup() {
 function draw() {
 
 }
+var num = 1;//first question on test
+if(num<4){
+  console.log("Peace");
+}
+console.log("War");
+
+for(var i = 14; i > 8; i--){
+  i = i - 4;
+  console.log(i);
+}
