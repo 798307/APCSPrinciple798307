e9880c66812c96ace5327f236f4046bc34b9390b
798307 <54591188+798307@users.noreply.github.com>
2019-09-17 17:49:13 -0700

    Paddle Game From Home

--- Projects/Project 913 Paddle Game/sketch.js
+++ Projects/Project 913 Paddle Game/sketch.js
@@ -14,11 +14,38 @@ function setup() {
 function Intro(){
   background(255,1,1);
   fill(0,0,0);
-  rect(200, 600, 100, 50, 20);
-  fill(0,0,0);
   textSize(80);
   text("Save The Balls", 130, 200);
+  for(var i = 0; i < 800; i = i+200){
+    fill(i/2,i/2,1/2);
+    rect(50+i, 600, 100, 50, 20);
   }
+  for(var i = 0; i < 200 ; i++){
+    fill(random(0,i+20), random(0,255-i), random(0, 1.5*i));
+    ellipse(width/2, height/2, 200,200);
+  }
+  for(var i = 50; i === 650; i = i + 200){
+    if(i===50){
+      textSize(10);
+      fill(255,255,255);
+      text("Easy", i, 610);
+    }else if (i===250) {
+      textSize(10);
+      fill(255,255,255);
+      text("Medium", i+50, 620);
+    }else if (i===460) {
+      textSize(10);
+      fill(255,255,255);
+      text("Hard", i+50, 620);
+    }else if (i===650) {
+      textSize(10);
+      fill(255,255,255);
+      text("Instruction", i+50, 620);
+    }
+  }
+
+
+}
 
 function Game(){
   background(20,20,20,20);
