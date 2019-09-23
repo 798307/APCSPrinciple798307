80c94d07e9087c469afc5130872f58630e85415f
798307 <54591188+798307@users.noreply.github.com>
2019-09-23 08:51:28 -0700

    923

--- Projects/Project 913 Paddle Game/sketch.js
+++ Projects/Project 913 Paddle Game/sketch.js
@@ -13,6 +13,20 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
 }
+function Instructions(){
+  background(255,255,255);
+  textSize(30);
+  fill(0,0,0);
+  text("Catch the balls before the time runs out.", (width/2) - 300, height/4);
+  rect((width/2) - 50, height/2, 100, 50);
+  fill(255,255,255);
+  textSize(20);
+  text("X Back", width/2 - 45, height/2 + 25);
+  if(mouseX > 350 && mouseX < 450 && mouseY > 400 && mouseY < 550 && mouseIsPressed){
+    clear();
+    gameState = 1;
+ }
+}
 
 function Intro(){
   background(0,0,255);
@@ -41,30 +55,34 @@ function Intro(){
       fill(255,255,255);
       text("Hard", i+20, 620);
     }else if (i===650) {
-      textSize(20);
+      textSize(18);
       fill(0, 0, 0);
       text("Instructions", i, 620);
     }
   }
-  if(mouseX > 50 && mouseX < 150 && mouseY > 600 && mouseY < 650){
+  if(mouseX > 50 && mouseX < 150 && mouseY > 600 && mouseY < 650 && mouseIsPressed){
     loadObjects(10);
     clear();
     gameState = 2;
     difficulty = 0;
     time = 150;
-  }else if(mouseX > 250 && mouseX < 350 && mouseY > 600 && mouseY < 650){
+  }else if(mouseX > 250 && mouseX < 350 && mouseY > 600 && mouseY < 650 && mouseIsPressed){
     loadObjects(20);
     clear();
     gameState = 2;
     difficulty = 1;
     time = 150;
-  }else if(mouseX > 450 && mouseX < 550 && mouseY > 600 && mouseY < 650){
+  }else if(mouseX > 450 && mouseX < 550 && mouseY > 600 && mouseY < 650 && mouseIsPressed){
     loadObjects(30);
     clear();
     gameState = 2;
     difficulty = 2;
     time = 150;
+  }else if(mouseX > 650 && mouseX < 750 && mouseY > 600 && mouseY < 650 && mouseIsPressed){
+    clear();
+    gameState = 4;
   }
+
 }
 
 function Game(){
@@ -73,16 +91,17 @@ function Game(){
   collision();
   textSize(30);
   fill(0,0,0);
-  text("Score = " + score, 40, 40 )
-  for(var i = 150; i > 0; i--){
+  text("Score = " + score, 40, 40 );
+  for(var i = 300; i > 0; i--){
     time = i
+    text("Time = "+ int(time/30), 600, 40);
   }
   if(difficulty === 0 && score === 10){
-    win = 'yes';
+    gameState = 3;
   }else if(difficulty === 1 && score === 20){
-    win = 'yes';
+    gameState = 3;
   }else if(difficulty === 2 && score === 30){
-    win = 'yes';
+    gameState = 3;
   }
   if(time === 0 && win === 'no'){
       gameState = 3;
@@ -90,11 +109,12 @@ function Game(){
   if(time === 0 && win === 'yes'){
     gameState = 3;
   }
-  console.log(time);
 }
 
 function EndGame(){
   background(255,255,255);
+  textSize(80);
+  text("Game Over", width/2 - 200, height/2);
 }
 
 //  The draw function is called @ 30 fps
@@ -107,6 +127,9 @@ function draw() {
   }
   if(gameState===3){
     EndGame();
+  }
+  if(gameState===4){
+    Instructions();
   }
 }
 
