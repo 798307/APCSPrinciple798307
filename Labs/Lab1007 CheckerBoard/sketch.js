c520f8f9d938548331942a42343ceeedeb82a930
798307 <54591188+798307@users.noreply.github.com>
2019-10-09 07:54:11 -0700

    1009

--- Labs/Lab1007 CheckerBoard/sketch.js
+++ Labs/Lab1007 CheckerBoard/sketch.js
@@ -2,10 +2,6 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var squares = [];
-var row = 0;
-var collumn = -1;
-var number = 0;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -15,23 +11,5 @@ function setup() {
 
 //  The draw function is called @ 30 fps
 function draw() {
-  runSquares();
-}
 
-function LoadSquares(){
-  for(var i = 0; i < 8; i = i + 100){
-    collumn = collumn + 1;
-    for(var j = 0; i < 8; j = j + 100){
-      squares[collumn] = new Square(j, i , 100, 100);
-      number = number + 1;
-    }
-    row = row + 1;
-  }
-}
-
-function runSquares(){
-  for(var i = 0; i <= squares.length; i++){
-    console.log(squares[i]);
-    squares[i].run();
-  }
 }
