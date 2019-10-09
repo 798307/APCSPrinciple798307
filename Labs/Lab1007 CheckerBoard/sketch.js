67b5f3fc32cfcb7baee8761ab4ea594a9d3a54bb
798307 <54591188+798307@users.noreply.github.com>
2019-10-09 08:50:29 -0700

    1009

--- Labs/Lab1007 CheckerBoard/sketch.js
+++ Labs/Lab1007 CheckerBoard/sketch.js
@@ -2,14 +2,25 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var squares = [];
+var rowNum = 0;
+var number = 0;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
+  LoadSquares();
 }
 
 //  The draw function is called @ 30 fps
-function draw() {
-
+function LoadSquares() {
+  for(var n = 0; n<8; n++){
+    rowNum  = n;
+    for(var i = 0; i<8; i++){
+      number = i;
+      squares[i] = new Square(100*i, 100*n);
+      squares[i].run();
+    }
+  }
 }
--- Labs/Lab1007 CheckerBoard/square.js
+++ Labs/Lab1007 CheckerBoard/square.js
@@ -1,26 +1,29 @@
 //Andrew Matel
 //10/7/19
 class Square{
-  constructor(x,y,w){
-    this.loc = CreateVector(x,y);
-    this.w = w;
+  constructor(x,y){
+    this.loc = createVector(x,y);
   }
   run(){
     this.render();
   }
 
   render(){
-    if(number % 2 === 0){
-      fill(0,0,0);
-    }else if(number % 2 === 1){
-      fill(255,255,255);
+    if(rowNum % 2 === 0){
+      if(number % 2 ===1){
+        fill(255,255,255);
+      }else{
+        fill(0);
+      }
     }
-    rect(this.loc.x, this.loc.y, this.w, this.w);
-    if(number % 2 === 0){
-      fill(0,0,0);
-    }else if(number % 2 === 1){
-      fill(255,255,255);
+    if(rowNum % 2 ===1){
+      if(number % 2 === 0){
+        fill(0);
+      }else{
+        fill(255,255,255);
+      }
     }
+    rect(this.loc.x, this.loc.y, 100,100);
   }
 
 }
