e7631cea24f5086fea95b82afd4a18563376397f
798307 <54591188+798307@users.noreply.github.com>
2019-10-08 21:35:15 -0700

    1008

--- Labs/Lab1007 CheckerBoard/square.js
+++ Labs/Lab1007 CheckerBoard/square.js
@@ -12,10 +12,15 @@ class Square{
   render(){
     if(number % 2 === 0){
       fill(0,0,0);
-    }else{
+    }else if(number % 2 === 1){
       fill(255,255,255);
     }
     rect(this.loc.x, this.loc.y, this.w, this.w);
+    if(number % 2 === 0){
+      fill(0,0,0);
+    }else if(number % 2 === 1){
+      fill(255,255,255);
+    }
   }
 
 }
