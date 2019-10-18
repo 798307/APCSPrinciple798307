9a9754d098b7631814640070dce7e90fb8740d12
798307 <54591188+798307@users.noreply.github.com>
2019-10-17 21:32:22 -0700

    Insertion Sort w/ simple list

--- Labs/Lab1017 Insertion Sort/sketch.js
+++ Labs/Lab1017 Insertion Sort/sketch.js
@@ -3,7 +3,7 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var n = 10000;
-var list = [];
+var list = [7,5,2,6,3,8,4,10,9,1];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -12,25 +12,16 @@ function setup() {
 
 
 }
-function swap();
-
 //  The draw function is called @ 30 fps
-function draw() {
-  for(var i = 0; i < n; i++){
-    var number = random(10);
-    list[i] = number;
-   }
-  for(var i = 0; i <= list.length-1; i++){
-    for(var j = list.length; j >= 0; j--){
-      if(list[j] > list[j-1] && list[j] < list[j+1]){
-
-      }else if(list[j-1] === NaN){
-
-      }else{
-        swap();
-      }
-
-    }
-  }
+function draw() {};
+console.log(list);
+const len = list.length;
+for (var i = 0; i < len; i++) {
+ var el = list[i];
+ var j;
+ for (j = i - 1; j >= 0 && list[j] > el; j--) {
+   list[j + 1] = list[j];
+ }
+ list[j + 1] = el;
 }
 console.log(list);
