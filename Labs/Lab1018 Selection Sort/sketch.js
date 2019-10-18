90aea32cbe0301c6c681808646d0f438e66cdfe9
798307 <54591188+798307@users.noreply.github.com>
2019-10-18 08:50:43 -0700

    1018 Select Sort

--- Labs/Lab1018 Selection Sort/sketch.js
+++ Labs/Lab1018 Selection Sort/sketch.js
@@ -2,27 +2,32 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var n = 10000;
-var list = [7,5,2,6,3,8,4,10,1,9];
+var lowest = 100000000000000000
+var list = [7,5,2,6,3,8,4,10,1,9]
+var newlist = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-}
-//  The draw function is called @ 30 fps
-function swap(a,x,y){
-  var temp = a[x];
-  a[x] = a[y];
-  a[y] = temp;
+
+
 }
 
-console.log(list);
-for(var i = 1; i < list.length; i++){
-  for(var j = i; j > 0; j--){
-    if(list[j] < list[j-1]){
-      swap(list,j,j-1);
+//  The draw function is called @ 30 fps
+function draw() {
+}
+
+for(var i = 0; i === list.length; i++){
+  i = 0:
+  for(var j = list.length; j > 0; j--){
+    if(list[j] < lowest){
+      lowest = list[j];
+      console.log(lowest);
     }
   }
+  list.splice(lowest);
+  newlist.push(lowest);
 }
 console.log(list);
+console.log(newlist);
