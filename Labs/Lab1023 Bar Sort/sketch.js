786b5a5918038b6611793eeb89a0a5361cd3ac97
798307 <54591188+798307@users.noreply.github.com>
2019-10-23 20:33:04 -0700

    Completed Bar Sort

--- Labs/Lab1023 Bar Sort/sketch.js
+++ Labs/Lab1023 Bar Sort/sketch.js
@@ -11,23 +11,41 @@ function setup() {
   fill(200, 30, 150);
   loadBars(100);
 }
+function swap(a,x,y){
+  frameRate(1);
+  var temp = a[x];
+  a[x] = a[y];
+  a[y] = temp;
+}
+
+function changelocation(l,x,y){
+  var temploc = l[x].loc.x;
+  l[x].loc.x = l[y].loc.x;
+  l[y].loc.x = temploc;
+}
 
 //  The draw function is called @ 30 fps
 function draw() {
-runballs();
-for(var i = 0; i < barlist.length; i++){
-  
-}
+  background(0,0,0);
+  runbars();
+  for(var i = barlist.length-1; i > 0; i--){
+    for(var j = 0; j < i; j++){
+      if(barlist[j].h > barlist[j+1].h){
+        swap(barlist, j, j+1);
+        changelocation(barlist, j, j+1);
+      }
+    }
+  }
 }
 
 function loadBars(){
   for(var i = 0; i < 100; i++){
     barlist[i] = new Bar(x, Bar.h);
-    x = x + 8
+    x = x + 8;
   }
 }
 
-function runballs(){
+function runbars(){
  for(var i = 0; i < barlist.length; i++){
    barlist[i].run();
  }
